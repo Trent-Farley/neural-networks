2430e64e641c9fc0f0c09412b00f8057023753f4
Trent-Farley <trent.a.farley@gmail.com>
2019-05-20 14:29:57 -0700

    first

---
+++ public/javascripts/flex.js
@@ -0,0 +1,22 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  var x = document.getElementById("main");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  var x = document.getElementById("main");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } 
+}
